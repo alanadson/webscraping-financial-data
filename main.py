4df9581b95b7d93a9e5d5456fc16b60bc4e5028a
Alan Adson <83378537+alanadson@users.noreply.github.com>
2022-09-19 17:12:40 -0300

    update

--- main.py
+++ main.py
@@ -1,23 +1,71 @@
 import requests
 import bs4
+import locale
 import tabulate
 
-headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+from models import RealEstateFund, Strategy
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+
+def percentage_change(percentage_str):
+    return locale.atof(percentage_str.split('%')[0])
+
+
+def decimal_change(decimal_str):
+    return locale.atof(decimal_str)
+
+
+headers = {
+    'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
 
 answer = requests.get('https://www.fundamentus.com.br/fii_resultado.php', headers=headers)
 
 soup = bs4.BeautifulSoup(answer.text, 'html.parser')
 
-print(soup.prettify())
-
 lines = soup.find(id='tabelaResultado').find('tbody').find_all('tr')
+
+result = []
+
+strategy = Strategy(
+    price_min=0,
+    dividend_yield_min=0,
+    p_vp_min=0,
+    market_value_min=0,
+    liquidity_min=0,
+    number_real_min=0,
+    average_vacancy_min=0
+)
 
 for line in lines:
     data = line.find_all('td')
-    print(
-        f"[{data[0].text}]\n"
-        f"Cotação: {data[2].text}\n"
-        f"Setor: {data[1].text}\n"
-        f"DY %: {data[4].text}\n"
-        f"P/VP: {data[5].text}\n"
+    paper = data[0].text
+    segment = data[1].text
+    price = decimal_change(data[2].text)
+    ffo_yield = percentage_change(data[3].text)
+    dividend_yield = percentage_change(data[4].text)
+    p_vp = decimal_change(data[5].text)
+    market_value = decimal_change(data[6].text)
+    liquidity = decimal_change(data[7].text)
+    number_real = int(data[8].text)
+    pricer_per_m2 = decimal_change(data[9].text)
+    rent_per_m2 = decimal_change(data[10].text)
+    cap_rate = percentage_change(data[11].text)
+    average_vacancy = percentage_change(data[12].text)
+
+    real_Estate_Fund = RealEstateFund(
+        paper, segment, price, ffo_yield, dividend_yield, p_vp, market_value, liquidity, number_real, pricer_per_m2,
+        rent_per_m2, cap_rate, average_vacancy
     )
+
+    if strategy.apply_strategy(real_Estate_Fund):
+        result.append(real_Estate_Fund)
+
+header = ['PAPER', 'SEGMENT', 'PRICE', 'DIVIDEND YIELD']
+
+table = []
+
+for elements in result:
+    table.append([elements.paper, elements.segment, locale.currency(elements.price), f"{locale.str(elements.dividend_yield)}%"])
+
+print(tabulate.tabulate(table, headers=header, tablefmt='fancy_grid'))
--- models.py
+++ models.py
@@ -1,15 +1,51 @@
 class RealEstateFund:
-    def __init__(self, paper, segment, price, ffo_yield, dividend_yield, p_vp, market_value, liquidity, number_real, pricer_per_m2, rent_per_m2, cap_rate, average_vacancy):
-        self.paper = paper,
-        self.segment = segment,
-        self.price = price,
+    def __init__(self, paper, segment, price, ffo_yield, dividend_yield, p_vp, market_value, liquidity, number_real,
+                 pricer_per_m2, rent_per_m2, cap_rate, average_vacancy):
+        self.paper = paper
+        self.segment = segment
+        self.price = price
         self.ffo_yield = ffo_yield
-        self.dividend_yield = dividend_yield,
-        self.p_vp = p_vp,
-        self.market_value = market_value,
-        self.liquidity = liquidity,
-        self.number_real = number_real,
-        self.pricer_per_m2 = pricer_per_m2,
-        self.rent_per_m2 = rent_per_m2,
-        self.cap_rate = cap_rate,
-        self.average_vacancy = avarage_vacancy
+        self.dividend_yield = dividend_yield
+        self.p_vp = p_vp
+        self.market_value = market_value
+        self.liquidity = liquidity
+        self.number_real = number_real
+        self.pricer_per_m2 = pricer_per_m2
+        self.rent_per_m2 = rent_per_m2
+        self.cap_rate = cap_rate
+        self.average_vacancy = average_vacancy
+
+
+class Strategy:
+    def __init__(self, segment="", price_min=0, ffo_yield_min=0, dividend_yield_min=0, p_vp_min=0, market_value_min=0,
+                 liquidity_min=0, number_real_min=0, pricer_per_m2_min=0, rent_per_m2_min=0, cap_rate_min=0,
+                 average_vacancy_min=0):
+        self.segment = segment
+        self.price_min = price_min
+        self.ffo_yield_min = ffo_yield_min
+        self.dividend_yield_min = dividend_yield_min
+        self.p_vp_min = p_vp_min
+        self.market_value_min = market_value_min
+        self.liquidity_min = liquidity_min
+        self.number_real_min = number_real_min
+        self.pricer_per_m2_min = pricer_per_m2_min
+        self.rent_per_m2_min = rent_per_m2_min,
+        self.cap_rate_min = cap_rate_min
+        self.average_vacancy_min = average_vacancy_min
+
+    def apply_strategy(self, fund: RealEstateFund):
+        if self.segment != "":
+            if fund.segment != self.segment:
+                return False
+        if fund.price < self.price_min \
+                or fund.ffo_yield < self.ffo_yield_min \
+                or fund.dividend_yield < self.dividend_yield_min \
+                or fund.p_vp < self.p_vp_min \
+                or fund.market_value < self.market_value_min \
+                or fund.liquidity < self.liquidity_min \
+                or fund.number_real < self.number_real_min \
+                or fund.pricer_per_m2 < self.pricer_per_m2_min \
+                or fund.cap_rate < self.cap_rate_min \
+                or fund.average_vacancy < self.average_vacancy_min:
+            return False
+        return True

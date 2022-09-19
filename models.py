2768586e3ca4e1d70280d1d26b844b05334407ea
Alan Adson <83378537+alanadson@users.noreply.github.com>
2022-09-19 02:33:10 -0300

    add models

---
+++ models.py
@@ -0,0 +1,15 @@
+class RealEstateFund:
+    def __init__(self, paper, segment, price, ffo_yield, dividend_yield, p_vp, market_value, liquidity, number_real, pricer_per_m2, rent_per_m2, cap_rate, average_vacancy):
+        self.paper = paper,
+        self.segment = segment,
+        self.price = price,
+        self.ffo_yield = ffo_yield
+        self.dividend_yield = dividend_yield,
+        self.p_vp = p_vp,
+        self.market_value = market_value,
+        self.liquidity = liquidity,
+        self.number_real = number_real,
+        self.pricer_per_m2 = pricer_per_m2,
+        self.rent_per_m2 = rent_per_m2,
+        self.cap_rate = cap_rate,
+        self.average_vacancy = avarage_vacancy
